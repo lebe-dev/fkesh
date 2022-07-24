85f37695203998aea6a2fe659544b249d16fe55a
Eugene Lebedev <duke.tougu@gmail.com>
2022-07-24 10:25:28 +0300

    Add EncodingError

--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,9 @@ pub enum FileCacheError {
     #[error("File cache error")]
     Default,
 
+    #[error(transparent)]
+    EncodingError(#[from] serde_json::Error),
+
     /// Represents all other cases of `std::io::Error`.
     #[error(transparent)]
     IOError(#[from] std::io::Error),
