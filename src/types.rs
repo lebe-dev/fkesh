f2b4e8d02576211a05ca7d8694f97b4423ae05a8
Eugene Lebedev <duke.tougu@gmail.com>
2022-07-24 10:25:48 +0300

    Add type OperationResult

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
 use crate::error::FileCacheError;
 
+pub type OperationResult<T> = Result<T, FileCacheError>;
 pub type EmptyResult = Result<(), FileCacheError>;
 pub type OptionalResult<T> = Result<Option<T>, FileCacheError>;
